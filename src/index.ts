055f3ca4983a034291d1950afbe6755b4f887c51
marcpascualsanchez <marcpascualsanchez@gmail.com>
2022-07-02 23:50:57 +0200

    Add goldberg sphere

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import { ArcRotateCamera, Engine, HemisphericLight, Mesh, MeshBuilder, Scene, Vector3 } from "babylonjs";
-import { addLabelToMesh } from "./gui";
 
 var canvas: any = document.getElementById("renderCanvas");
 var engine: Engine = new Engine(canvas, true);
+const planetName = 'planet';
 
 function createScene(): Scene {
     var scene: Scene = new Scene(engine);
 
-    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+    var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 6, Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
-    var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+    new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
 
-    var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, scene);
-
-    addLabelToMesh(sphere);
+    const g = BABYLON.MeshBuilder.CreateGoldberg(planetName, { m: 3, n: 2 });
 
     return scene;
 }
@@ -23,4 +21,6 @@ var scene: Scene = createScene();
 
 engine.runRenderLoop(() => {
     scene.render();
+    const planet = scene.getMeshByName(planetName);
+    planet?.rotate(new Vector3(0, 1, 0), 0.005);
 });
