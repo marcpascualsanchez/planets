66e50c9415d09ae7839cc525002a378f8c2e07eb
marcpascualsanchez <marcpascualsanchez@gmail.com>
2022-07-02 23:40:51 +0200

    Add babylon + webpack + hotreload devserver

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: "ts-loader", exclude: '/node_modules/' }
+        ]
+    },
+    externals: {
+        "oimo": true,
+        "cannon": true,
+        "earcut": true
+    },
+    mode: "development",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        compress: true,
+        port: 9000,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
